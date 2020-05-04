48ba115af840d25900525f09c65f901a7a651e04
lkyi <269577519@qq.com>
2020-05-04 20:31:49 +0800

    feat: 添加rabbitmq部分

---
+++ Project-SpringBoot-1-rabbitmq/SpringBoot-rabbitmq-consumer/src/test/java/com/www/lkyi/SpringBootRabbitmqConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.www.lkyi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRabbitmqConsumerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Project-SpringBoot-1-rabbitmq/SpringBoot-rabbitmq-producer/src/main/java/com/www/lkyi/SpringBootRabbitmqProducerApplication.java
@@ -0,0 +1,13 @@
+package com.www.lkyi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRabbitmqProducerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootRabbitmqProducerApplication.class, args);
+    }
+
+}
