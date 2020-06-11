abfd59435da20725e36764d7ea38c24f7a15ffe2
lkyi <lkyi@senthink.com>
2020-06-11 18:01:25 +0800

    feat：添加seata项目

--- Project-SpringBoot-0-init/src/main/java/com/lkyi/ProjectSpringBoot0InitApplication.java
+++ Project-SpringBoot-0-init/src/main/java/com/lkyi/ProjectSpringBoot0InitApplication.java
@@ -3,9 +3,11 @@ package com.lkyi;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @MapperScan("com.lkyi.dao")
+@EnableDiscoveryClient
 public class ProjectSpringBoot0InitApplication {
 
     public static void main(String[] args) {
